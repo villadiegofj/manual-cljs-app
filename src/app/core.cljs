446b45a0ad0258753a9f4c915dbbf2435b51ed63
Franky Villadiego <fvilladiego@emida.net>
2021-12-10 18:47:46 -0500

    Step two. Script. Dinamic html.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,8 +1,22 @@
 (ns app.core)
 
+; application STATE
+(defonce title (atom "HOLA MUNDO!!"))
+
+; get main div
+(defn app []
+  (.getElementById js/document "app"))
+
+; create an HTML element
+(defn h1 []
+  (doto
+    (.createElement js/document "H1")
+    (aset "innerHTML" @title)))
+
 ; meta tag :dev/after-load is for hot-code reloading
 (defn ^:dev/after-load start []
-  (js/console.log "Hello CLJS"))
+  (js/console.log "Hello CLJS 2")
+  (.appendChild (app) (h1)))
 
 ; entry point and called once
 (defn ^:export init []
