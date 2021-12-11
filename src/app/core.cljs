8e81362371c487a099daa3c90e601c4c7ba9d7ed
Franky Villadiego <fvilladiego@emida.net>
2021-12-10 20:29:44 -0500

    Step 04. Counter component.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,24 +1,25 @@
 (ns app.core
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rd]))
 
 ; application STATE
-(defonce title (atom "HOLA MUNDO!!"))
+(defonce counter (r/atom 0))
 
 ; get main div
 (defn app []
   (.getElementById js/document "app"))
 
 ; create an HTML element
-(defn simple-component []
-  [:div
-    [:p "Hello Reagent"]])
+(defn counter-component []
+  [:div "Counter No: " @counter
+   [:br]
+   [:button {:on-click #(swap! counter inc)} "Click Me"]])
 
 ; meta tag :dev/after-load is for hot-code reloading
 (defn ^:dev/after-load start
   "Render the toplevel component for this app."
   []
-  (rdom/render [simple-component] (app)))
+  (rd/render [counter-component] (app)))
 
 ; entry point and called once
 (defn ^:export init []
