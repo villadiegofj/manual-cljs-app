003998a5e0193442c1ab7569837540f935675f42
Franky Villadiego <fvilladiego@emida.net>
2021-12-11 15:31:20 -0500

    Step 07. A mirror for github.com/gothinkster/react-redux-realworld-example-app

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -5,24 +5,56 @@
 ; application STATE
 (defonce counter (r/atom 0))
 
-; get main div
-(defn app []
-  (.getElementById js/document "app"))
+(defonce mock-articles
+   [{:title "Backpacking is fun"}])
 
-; create an HTML element
-(defn counter-component []
-  [:div {:class ["content"]}
-   "Counter No: " @counter
-   [:br]
-   [:button {:on-click #(swap! counter inc)
-             :class ["button" "is-primary"]
-             } "Click Me"]])
+(defn articles [items]
+  (if-not (seq items)
+    [:div.article-preview "Loading..."]
+    (if (= 0 (count items))
+      [:div.article-preview "No articles are here."]
+      [:div
+       (for [article items]
+          [:h2 (:title article)])])))
+
+(defn header []
+  [:nav.navbar.navbar-light
+   [:div.container]
+   [:a.navbar-brand "conduit"]])
+
+(defn banner [token]
+  (when token
+    [:div.banner>div.container
+     [:h1.logo-front "conduit"]
+     [:p "A place to share your knowledge"]]))
+
+(defn main-view []
+  [:div.col-md-9
+   [:div.feed-toggle
+    [:ul.nav.nav-pills.outline-active
+     [:li.nav-item
+      [:a.nav-link.active {:href ""} "Global feed"]]]]
+   [articles mock-articles]])
+
+(defn home-page []
+  [:div.home-page
+   [banner "auth-user-token"]
+   [:div.container.page>div.row
+    [main-view]
+    [:div.col-md-3
+     [:div.sidebar
+      [:p "Popular tags"]]]]])
+
+(defn app []
+  [:div
+   [header]
+   [home-page]])
 
 ; meta tag :dev/after-load is for hot-code reloading
 (defn ^:dev/after-load start
   "Render the toplevel component for this app."
   []
-  (rd/render [counter-component] (app)))
+  (rd/render [app] (.getElementById js/document "app")))
 
 ; entry point and called once
 (defn ^:export init []
