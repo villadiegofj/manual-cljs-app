8b9b79d75da4541e8a3087ed8a85e36dccf080ca
Franky Villadiego <fvilladiego@emida.net>
2021-12-23 12:45:52 -0500

    Step 18: Register handler with API.

--- src/app/auth.cljs
+++ src/app/auth.cljs
@@ -1,26 +1,32 @@
 (ns app.auth
   (:require [reagent.core :as r]
-            [app.api :refer (api-url error-handler)]
+            [app.api :refer (api-url)]
             [ajax.core :refer (POST json-request-format json-response-format)]))
 
 (defonce auth-state (r/atom nil))
+(defonce error-state (r/atom nil))
 
 (defn auth-success! [{{:keys [token] :as user} :user}]
   (.setItem js/localStorage "auth-token" token)
-  (reset! auth-state user))
+  (reset! auth-state user)
+  (reset! error-state nil))
+
+(defn auth-error! [{{:keys [errors]} :response} ]
+  (reset! error-state errors))
+
 
 (defn register! [register-input]
   (POST (str api-url "/users") {:params {:user register-input}
                                 :format (json-request-format)
                                 :handler auth-success!
-                                :error-handler error-handler
+                                :error-handler auth-error!
                                 :response-format (json-response-format {:keywords? true})}))
 
 (defn login! [login-input]
   (POST (str api-url "/users/login") {:params {:user login-input}
                                 :format (json-request-format)
                                 :handler auth-success!
-                                :error-handler error-handler
+                                :error-handler auth-error!
                                 :response-format (json-response-format {:keywords? true})}))
 
 
@@ -33,6 +39,7 @@
            :password "12345"})
 
   (:user (deref auth-state))
+  (deref error-state)
   (.setItem js/localStorage "auth-token" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpbGxhLjAyQGNvbmRvci5jb20iLCJ1c2VybmFtZSI6InZpbGxhLjAyQGNvbmRvci5jb20iLCJiaW8iOm51bGwsImltYWdlIjoiaHR0cHM6Ly9hcGkucmVhbHdvcmxkLmlvL2ltYWdlcy9zbWlsZXktY3lydXMuanBlZyIsImlhdCI6MTYzOTk2MTg3OSwiZXhwIjoxNjQ1MTQ1ODc5fQ.nHisPAUt7y3-hNH7LI5hEEFknPqez5SI3937iLFpLSw")
   (.getItem js/localStorage "auth-token")
   )
--- src/app/pages/register.cljs
+++ src/app/pages/register.cljs
@@ -1,25 +1,52 @@
 (ns app.pages.register
-  (:require [app.auth :as auth]
-            [reitit.frontend.easy :as rfe]))
+  (:require [app.auth :as auth :refer (error-state)]
+            [reitit.frontend.easy :as rfe]
+            [reagent.core :as r]
+            [clojure.string :as str]))
 
 (defn register! [evt input]
   (.preventDefault evt)
   (auth/register! input))
 
+(defn list-errors [errors]
+  (when (seq errors)
+    [:ul.error-messages
+     (for [[key value] errors]
+       [:li
+        (-> key
+            name
+            str/capitalize
+            (str " - " (str/join ", " value)))])]))
+
 (defn register-page []
-  [:div.auth-page>div.container.page>div.row
-   [:div.col-md-6.offset-md-3.col-xs-12
-    [:h1.text-xs-center "Sign Up"]
-    [:p.text-xs-center [:a {:href (rfe/href :login)} "Have an account?"]]
-    [:form {:on-submit #(register! % {})}
-     [:fieldset
-      [:fieldset.form-group
-       [:input.form-control.form-control-lg
-        {:type "text" :placeholder "Username"}]]
-      [:fieldset.form-group
-       [:input.form-control.form-control-lg
-        {:type "email" :placeholder "Email"}]]
-      [:fieldset.form-group
-       [:input.form-control.form-control-lg
-        {:type "password" :placeholder "Password"}]]
-      [:button.btn.btn-lg.btn-primary.pull-xs-right "Sign Up"]]]]])
+  (let [initial-state {:username ""
+                       :password ""
+                       :email ""}
+        state (r/atom initial-state)]
+    (fn []
+      [:div.auth-page>div.container.page>div.row
+       [:div.col-md-6.offset-md-3.col-xs-12
+        [:h1.text-xs-center "Sign Up"]
+        [:p.text-xs-center [:a {:href (rfe/href :login)} "Have an account?"]]
+        [list-errors @error-state]
+        [:form {:on-submit #(register! % @state)}
+         [:fieldset
+          [:fieldset.form-group
+           [:input.form-control.form-control-lg
+            {:type "text"
+             :placeholder "Username"
+             :value (:username @state)
+             :on-change #(swap! state assoc :username (.. % -target -value))}]]
+          [:fieldset.form-group
+           [:input.form-control.form-control-lg
+            {:type "email"
+             :placeholder "Email"
+             :value (:email @state)
+             :on-change #(swap! state assoc :email (.. % -target -value))}]]
+          [:fieldset.form-group
+           [:input.form-control.form-control-lg
+            {:type "password"
+             :placeholder "Password"
+             :value (:password @state)
+             :on-change #(swap! state assoc :password (.. % -target -value))}]]
+          [:button.btn.btn-lg.btn-primary.pull-xs-right "Sign Up"]]]]])))
